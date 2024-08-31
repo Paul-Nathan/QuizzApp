7d5340f2b63e01a234d1541e3f2307b4e5ef0c24
Paul-Nathan <pngotte@gmail.com>
2024-08-31 05:10:52 +0100

    Fetching Data from Quiz API

--- screens/QuizScreen.js
+++ screens/QuizScreen.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+const apiURL = "https://opentdb.com/api.php?amount=10&category=18&type=multiple"
+
 export default class QuizScreen extends React.Component {
+
+    componentDidMount() {
+        this.fetchQuestion()
+    }
+
+    async fetchQuestion() {
+        return fetch(apiURL)
+        .then(result=>{
+
+            result.json().then(resultJSON=>{
+                console.log(resultJSON)
+            })
+
+        })
+        .catch(error=>console.log(error))
+    }
 
   render() {
     return(
